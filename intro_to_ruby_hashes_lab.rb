c0663bcb6cefb920a0d20b5622e2bd91bf3880c8
Henry Phan <h.phan06@gmail.com>
2019-11-13 00:25:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,14 +11,17 @@ end
 
 def monopoly
 	monopoly = {
-	  :railroads=> {}
+	  :railroads => {}
 	}
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {}
+	  :railroads => {
+	    :pieces => 4,
+	    :rent_in_dollars =>
+	  }
 	}
 end
 
