51b768fcea8092ca297af7d57a510ab5f0f4a8f9
Henry Phan <h.phan06@gmail.com>
2019-11-13 01:05:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,9 @@ def monopoly_with_third_tier
 	  {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {},
+	      :reading_railroad => {
+	        "mortgage_value =>"
+	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
